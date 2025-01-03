76a6565ed965d8577ad592feb0d160d0d68c7c69
longdinh-theinfitech <long_dinh@theinfitech.com>
2025-01-03 13:14:39 +0000

    fix null variable

--- api/api_v1/lookback/routers/lookback.py
+++ api/api_v1/lookback/routers/lookback.py
@@ -15,10 +15,10 @@ router = APIRouter()
 @router.post("/generate_lookback", response_model=LookbackResponse)
 async def generate_lookback(request: LookbackRequest, db: Session = Depends(get_master_db)):
     limit_accounts = 5
-    twitter_account = get_user_detail_by_screen_name(db, request.screen_name)
     query = db.exec(select(Lookback).where(Lookback.screen_name == request.screen_name)).first()
 
     if query:
+        twitter_account = get_user_detail_by_screen_name(db, request.screen_name)
         return LookbackResponse(
             name=twitter_account.name,
             screen_name=twitter_account.screen_name,
@@ -31,20 +31,18 @@ async def generate_lookback(request: LookbackRequest, db: Session = Depends(get_
     result = crawl_service.crawl_tweets(db, request.screen_name, limit_accounts)
     response = None
     start_time = time.time()
-    while response is None and (time.time() - start_time) < 60:
-        query = db.exec(select(Lookback).where(Lookback.screen_name == request.screen_name))
-        response = query.first()
+    while response is None and (time.time() - start_time) < 40:
+        response = db.exec(select(Lookback).where(Lookback.screen_name == request.screen_name)).first()
         time.sleep(0.5)
-
     try:
-        message = response.model_dump()
+        twitter_account = get_user_detail_by_screen_name(db, request.screen_name)
         return LookbackResponse(
             name=twitter_account.name,
             screen_name=twitter_account.screen_name,
             followers_count=twitter_account.followers_count,
             following_count=twitter_account.following_count,
             is_blue_verified=twitter_account.is_blue_verified,
-            **json.loads(message["lookback_msg"])
+            **json.loads(response.model_dump()["lookback_msg"])
         )
     except Exception as e:
         return {}
